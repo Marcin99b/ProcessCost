79a4e4574cd35783331ee8fb6bd9322423bb9e72
Marcin99b <marcin.bigos99@gmail.com>
2023-12-18 02:16:17 +0100

    move entities to files

--- ProcessCost.Database/Entities/StageGroupEntity.cs
+++ ProcessCost.Database/Entities/StageGroupEntity.cs
@@ -1,9 +1,8 @@
 ﻿namespace ProcessCost.Database.Entities;
 
-public record StageEntity : Entity
+public record StageGroupEntity : Entity
 {
     public string Name { get; init; }
-    public int Day { get; init; }
     public int MoneyAmount { get; init; }
     public string MoneyCurrency { get; init; }
 }
---
+++ ProcessCost.Database/Entities/StageGroupReferenceEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace ProcessCost.Database.Entities;
+
+public record StageGroupReferenceEntity : Entity
+{
+    public Guid StageId { get; init; }
+    public Guid StageGroupId { get; init; }
+}
