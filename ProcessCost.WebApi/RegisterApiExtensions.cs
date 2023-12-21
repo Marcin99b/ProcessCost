ca11b84d721ab6fd0da73f541b29e2aba261b996
Marcin99b <marcin.bigos99@gmail.com>
2023-12-21 09:01:10 +0100

    refactor register api

--- ProcessCost.WebApi/RegisterApiExtensions.cs
+++ ProcessCost.WebApi/RegisterApiExtensions.cs
@@ -20,62 +20,102 @@ public static class RegisterApiExtensions
 
     public static WebApplication SetupStagesApiV1(this WebApplication app)
     {
+        return app
+            .GetStagesV1()
+            .AddStageV1()
+            .UpdateStageMoneyV1()
+            .GetStateAtSelectedDayV1()
+            .CreateStageGroupV1()
+            .DeleteStageGroupV1()
+            .AddStageToGroupV1()
+            .RemoveStageFromGroupV1();
+    }
+
+    private static WebApplication GetStagesV1(this WebApplication app)
+    {
         app.MapGet(
                 $"/{Version10}/stages",
                 ([FromServices] IMediator mediator) =>
                     mediator.Send(new GetStagesRequest()))
             .WithMetadata(new SwaggerOperationAttribute(description: "Get list of all stage"))
             .WithTags(GroupStages);
+        return app;
+    }
 
+    private static WebApplication AddStageV1(this WebApplication app)
+    {
         app.MapPost(
                 $"/{Version10}/stages",
                 ([FromServices] IMediator mediator, [FromBody] AddStageRequest request) =>
                     mediator.Send(request))
             .WithMetadata(new SwaggerOperationAttribute(description: "Create new stage"))
             .WithTags(GroupStages);
+        return app;
+    }
 
+    private static WebApplication UpdateStageMoneyV1(this WebApplication app)
+    {
         app.MapPost(
                 $"/{Version10}/stages/money",
                 ([FromServices] IMediator mediator, [FromBody] UpdateStageMoneyRequest request) =>
                     mediator.Send(request))
             .WithMetadata(new SwaggerOperationAttribute(description: "Update money in selected stage and dependencies"))
             .WithTags(GroupStages);
+        return app;
+    }
 
+    private static WebApplication GetStateAtSelectedDayV1(this WebApplication app)
+    {
         app.MapGet(
                 $"/{Version10}/state/{{day:int}}",
                 ([FromServices] IMediator mediator, int day) =>
                     mediator.Send(new GetStateAtSelectedDayRequest(day)))
             .WithMetadata(new SwaggerOperationAttribute(description: "Get money balance of selected day"))
             .WithTags(GroupCalculations);
+        return app;
+    }
 
+    private static WebApplication CreateStageGroupV1(this WebApplication app)
+    {
         app.MapPost(
                 $"/{Version10}/stages/groups",
                 ([FromServices] IMediator mediator, [FromBody] CreateStageGroupRequest request) =>
                     mediator.Send(request))
             .WithMetadata(new SwaggerOperationAttribute(description: "Create new empty group"))
             .WithTags(GroupStagesGroups);
+        return app;
+    }
 
+    private static WebApplication DeleteStageGroupV1(this WebApplication app)
+    {
         app.MapDelete(
                 $"/{Version10}/stages/groups/{{groupId:guid}}",
                 ([FromServices] IMediator mediator, Guid groupId) =>
                     mediator.Send(new DeleteStageGroupRequest(groupId)))
             .WithMetadata(new SwaggerOperationAttribute(description: "Delete group with all references"))
             .WithTags(GroupStagesGroups);
+        return app;
+    }
 
+    private static WebApplication AddStageToGroupV1(this WebApplication app)
+    {
         app.MapPost(
                 $"/{Version10}/stages/groups/add",
                 ([FromServices] IMediator mediator, [FromBody] AddStageToGroupRequest request) =>
                     mediator.Send(request))
             .WithMetadata(new SwaggerOperationAttribute(description: "Add selected stage to selected group"))
             .WithTags(GroupStagesGroups);
+        return app;
+    }
 
+    private static WebApplication RemoveStageFromGroupV1(this WebApplication app)
+    {
         app.MapPost(
                 $"/{Version10}/stages/groups/remove",
                 ([FromServices] IMediator mediator, [FromBody] RemoveStageFromGroupRequest request) =>
                     mediator.Send(request))
             .WithMetadata(new SwaggerOperationAttribute(description: "Remove selected stage from selected group"))
             .WithTags(GroupStagesGroups);
-
         return app;
     }
 
