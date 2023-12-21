451a7cab0905a36e0b3789c2a028434e3802e0d7
Marcin99b <marcin.bigos99@gmail.com>
2023-12-21 09:50:50 +0100

    delete stage basic handler

---
+++ ProcessCost.Domain/Handlers/DeleteStageHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace ProcessCost.Domain.Handlers;
+
+public record DeleteStageRequest(Guid StageId) : IRequest<DeleteStageResponse>;
+
+public record DeleteStageResponse;
+
+public class DeleteStageHandler(IStagesRepository stagesRepository) : IRequestHandler<DeleteStageRequest, DeleteStageResponse>
+{
+    public async Task<DeleteStageResponse> Handle(DeleteStageRequest request, CancellationToken cancellationToken)
+    {
+        await stagesRepository.Delete(request.StageId);
+        return new();
+    }
+}
