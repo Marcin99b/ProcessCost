bef2d44b8ac9a471316860d09abf1225920e966b
Marcin99b <marcin.bigos99@gmail.com>
2023-12-21 07:21:00 +0100

    add stage handler

---
+++ ProcessCost.Domain/Handlers/AddStageHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using ProcessCost.Domain.Models;
+
+namespace ProcessCost.Domain.Handlers;
+
+public record AddStageRequest(string Name, int Day, decimal Amount, string Currency) : IRequest<AddStageResponse>;
+
+public record AddStageResponse;
+
+public class AddStageHandler(IStagesRepository stagesRepository) : IRequestHandler<AddStageRequest, AddStageResponse>
+{
+    public async Task<AddStageResponse> Handle(AddStageRequest request, CancellationToken cancellationToken)
+    {
+        var stage = new Stage(request.Name, request.Day, new (request.Amount, Enum.Parse<Currency>(request.Currency)));
+        await stagesRepository.Add(stage);
+        return new();
+    }
+}
